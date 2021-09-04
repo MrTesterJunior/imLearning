5e56a915b838792ee2caa0e5b9ac980d8922f67a
MrTesterJunior <pollamidol@gmail.com>
2021-09-04 19:20:09 +0200

    Intro a los objetos

---
+++ myTurtle.py
@@ -0,0 +1,15 @@
+import turtle
+
+baby_turtle = turtle.Turtle()
+brother_turtle = turtle.Turtle()
+
+brother_turtle.color('red')
+brother_turtle.shape('square')
+baby_turtle.shape('turtle')
+
+baby_turtle.fd(50)
+brother_turtle.left(90)
+brother_turtle.fd(50)
+
+while True:
+	pass
