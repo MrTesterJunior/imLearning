768b3bfbe557a9cda372cdfa98a93e934dfa6cb5
MrTesterJunior <pollamidol@gmail.com>
2021-09-04 10:41:27 +0200

    Funciones clasicas

--- sumatodos.py
+++ sumatodos.py
@@ -5,3 +5,18 @@ def sumaTodos(limitTo):
     return resultado
     
 print(sumaTodos(100))
+
+# Funciones de primera clase
+addAll = sumaTodos # Permiten asignarse a variables (entre otras opciones)
+
+print(addAll(4))
+
+# Función de nivel superior: Permite recibir funciones como parámetros o devolver una función
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado =+ i*i
+    return resultado
+    
+print(sumaTodosLosCuadrados(3))
+
